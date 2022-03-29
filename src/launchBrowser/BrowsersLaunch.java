d8331524a51f03815f995c12131bc70916b4e55a
rohinibehera <102520425+rohinibehera@users.noreply.github.com>
2022-03-29 23:25:37 +0530

    Search Hello
    Enter

--- src/launchBrowser/BrowsersLaunch.java
+++ src/launchBrowser/BrowsersLaunch.java
@@ -1,5 +1,7 @@
 package launchBrowser;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,10 +14,14 @@ public class BrowsersLaunch {
 		System.setProperty("webdriver.chrome.driver", "D:\\LIB\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("http://www.techlearn.in");
+		driver.get("https://www.google.com");
 		driver.navigate().back();
 		driver.navigate().forward();
 		driver.navigate().refresh();
+		driver.findElement(By.name("q")).sendKeys("Hello");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		
+	
 	}
 	
 
